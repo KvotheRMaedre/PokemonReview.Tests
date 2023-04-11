cca3317cef4f7b74a6be6d75e2dfc3e170ecd899
Ítalo H <italodssouza.backup@gmail.com>
2023-04-11 09:40:02 -0300

    Create CategoryControllerTest.cs
    Creating test for GetCategories

---
+++ Controller/CategoryControllerTest.cs
@@ -0,0 +1,67 @@
+﻿using AutoMapper;
+using FakeItEasy;
+using FluentAssertions;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using PokemonReview.Controllers;
+using PokemonReview.Dto;
+using PokemonReview.Interfaces;
+using PokemonReview.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PokemonReview.Tests.Controller
+{
+    public class CategoryControllerTest : IDisposable
+    {
+        private readonly ICategoryRepository _categoryRepository;
+        private readonly IMapper _mapper;
+        private readonly CategoryController _categoryController;
+
+        public CategoryControllerTest()
+        {
+            _categoryRepository = A.Fake<ICategoryRepository>();
+            _mapper = A.Fake<IMapper>();
+            _categoryController = new CategoryController(_categoryRepository, _mapper);
+        }
+
+        public void Dispose()
+        {
+            _categoryController.Dispose();
+        }
+
+        [Fact]
+        public void CategoryController_GetCategories_ReturnBadRequest()
+        {
+            //Arrange
+            _categoryController.ModelState.AddModelError("test", "test");
+
+            //Act
+            var result = _categoryController.GetCategories();
+
+            //Assert
+            result.Should().NotBeNull();
+            result.Should().BeOfType(typeof(BadRequestObjectResult));
+        }
+
+        [Fact]
+        public void CategoryController_GetCategories_ReturnOk()
+        {
+            var categoriesList = A.Fake<List<CategoryDto>>();
+            var categories = A.Fake<ICollection<Category>>();
+
+            //Arrange
+            A.CallTo(() => _categoryRepository.GetCategories()).Returns(categories);
+
+            //Act
+            var result = _categoryController.GetCategories();
+
+            //Assert
+            result.Should().NotBeNull();
+            result.Should().BeOfType(typeof(OkObjectResult));
+        }
+    }
+}
